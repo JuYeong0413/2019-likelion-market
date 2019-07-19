d35af57cee62d76db39c20b30f9eb600a442d9a1
JuYeong0413 <2jy22@naver.com>
2019-07-20 03:25:07 +0900

    add gitignore

--- project/settings.py
+++ project/settings.py
@@ -20,10 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '&ypn=y8ma5tt_1#h@k&e9&e@(omgl&j$j2v&tegt_a9n14n7sw'
+#SECRET_KEY = '&ypn=y8ma5tt_1#h@k&e9&e@(omgl&j$j2v&tegt_a9n14n7sw'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 
 ALLOWED_HOSTS = ['*']
 
